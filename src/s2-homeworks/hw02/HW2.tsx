c713e7c6ded0b91cc0ed01f49e02ebb3261f1e29
lBeMbOl <lbembol@mail.ru>
2024-03-09 18:17:06 +0500

    HW-2 Сделан

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -1,10 +1,10 @@
-import React, {useState} from 'react'
-import Affairs from './affairs/Affairs'
-import s2 from '../../s1-main/App.module.css'
+import React, { useState } from "react";
+import Affairs from "./affairs/Affairs";
+import s2 from "../../s1-main/App.module.css";
 
 /*
-* 1 - описать типы AffairPriorityType, AffairType
-* 2 - указать нужный тип для defaultAffairs
+
+
 * 3 - дописать типы и логику функции filterAffairs и проверить её тестами
 * 4 - выполнить пункт 3 для функции deleteAffair
 * 5 - указать нужный тип в useState с affairs
@@ -17,56 +17,60 @@ import s2 from '../../s1-main/App.module.css'
 * */
 
 // types
-export type AffairPriorityType = any // need to fix any
+export type AffairPriorityType = "low" | "middle" | "high";
 export type AffairType = {
-    _id: any // need to fix any
-    name: any // need to fix any
-    priority: AffairPriorityType
-}
-export type FilterType = 'all' | AffairPriorityType
+  _id: number; // need to fix any
+  name: string; // need to fix any
+  priority: AffairPriorityType;
+};
+export type FilterType = "all" | AffairPriorityType;
 
 // constants
-const defaultAffairs: any = [ // need to fix any
-    {_id: 1, name: 'React', priority: 'high'}, // студенты могут изменить содержимое name и количество элементов в массиве, ...priority не менять!
-    {_id: 2, name: 'anime', priority: 'low'},
-    {_id: 3, name: 'games', priority: 'low'},
-    {_id: 4, name: 'work', priority: 'high'},
-    {_id: 5, name: 'html & css', priority: 'middle'},
-]
+const defaultAffairs: AffairType[] = [
+  // need to fix any
+  { _id: 1, name: "React", priority: "high" }, // студенты могут изменить содержимое name и количество элементов в массиве, ...priority не менять!
+  { _id: 2, name: "anime", priority: "low" },
+  { _id: 3, name: "games", priority: "low" },
+  { _id: 4, name: "work", priority: "high" },
+  { _id: 5, name: "html & css", priority: "middle" },
+];
 
 // pure helper functions
-export const filterAffairs = (affairs: any, filter: any): any => { // need to fix any
-
-
-    return affairs // need to fix
-}
-export const deleteAffair = (affairs: any, _id: any): any => { // need to fix any
-
-    return affairs // need to fix
-}
+export const filterAffairs = (
+  affairs: AffairType[],
+  filter: FilterType
+): AffairType[] => {
+  return affairs.filter((t) => filter == "all" || t.priority == filter);
+};
+export const deleteAffair = (
+  affairs: AffairType[],
+  _id: number
+): AffairType[] => {
+  return affairs.filter((t) => t._id !== _id);
+};
 
 function HW2() {
-    const [affairs, setAffairs] = useState<any>(defaultAffairs) // need to fix any
-    const [filter, setFilter] = useState<FilterType>('all')
+  const [affairs, setAffairs] = useState<AffairType[]>(defaultAffairs); // need to fix any
+  const [filter, setFilter] = useState<FilterType>("all");
 
-    const filteredAffairs = filterAffairs(affairs, filter)
-    const deleteAffairCallback = (_id: any) => { // need to fix any
-        // need to fix
-    }
+  const filteredAffairs = filterAffairs(affairs, filter);
+  const deleteAffairCallback = (_id: number) => {
+    setAffairs(deleteAffair(affairs, _id));
+  };
 
-    return (
-        <div id={'hw2'}>
-            <div className={s2.hwTitle}>Homework #2</div>
-            <div className={s2.hw}>
-                <Affairs
-                    data={filteredAffairs}
-                    setFilter={setFilter}
-                    deleteAffairCallback={deleteAffairCallback}
-                    filter={filter}
-                />
-            </div>
-        </div>
-    )
+  return (
+    <div id={"hw2"}>
+      <div className={s2.hwTitle}>Homework #2</div>
+      <div className={s2.hw}>
+        <Affairs
+          data={filteredAffairs}
+          setFilter={setFilter}
+          deleteAffairCallback={deleteAffairCallback}
+          filter={filter}
+        />
+      </div>
+    </div>
+  );
 }
 
-export default HW2
+export default HW2;
