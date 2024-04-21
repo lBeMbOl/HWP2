679b07e843fe06ba8147a32195b41ef98dd612fb
lBeMbOl <lbembol@mail.ru>
2024-04-21 20:08:31 +0500

    HW6

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -16,38 +16,40 @@ export const Sidebar: FC<PropsType> = ({ open, handleClose }) => {
       {/*затемнение справа от открытого меню*/}
       {open && <div className={s.background} onClick={handleClose} />}
 
-      <aside className={sidebarClass}>
-        <button className={s.close} onClick={handleClose}>
-          <img src={closeIcon} alt="close sidebar" id={"hw5-menu-close"} />
-        </button>
+      {open && (
+        <aside className={sidebarClass}>
+          <button className={s.close} onClick={handleClose}>
+            <img src={closeIcon} alt="close sidebar" id={"hw5-menu-close"} />
+          </button>
 
-        <nav id={"hw5-menu"} className={s.nav}>
-          <NavLink
-            id={"hw5-pre-junior-link"}
-            to={PATH.PRE_JUNIOR}
-            onClick={handleClose}
-            className={({ isActive }) => (isActive ? s.active : "")}
-          >
-            Pre-junior
-          </NavLink>
-          <NavLink
-            id={"hw5-junior-link"}
-            to={PATH.JUNIOR}
-            onClick={handleClose}
-            className={({ isActive }) => (isActive ? s.active : "")}
-          >
-            Junior
-          </NavLink>
-          <NavLink
-            id={"hw5-junior-plus-link"}
-            to={PATH.JUNIOR_PLUS}
-            onClick={handleClose}
-            className={({ isActive }) => (isActive ? s.active : "")}
-          >
-            Junior Plus
-          </NavLink>
-        </nav>
-      </aside>
+          <nav id={"hw5-menu"} className={s.nav}>
+            <NavLink
+              id={"hw5-pre-junior-link"}
+              to={PATH.PRE_JUNIOR}
+              onClick={handleClose}
+              className={({ isActive }) => (isActive ? s.active : "")}
+            >
+              Pre-junior
+            </NavLink>
+            <NavLink
+              id={"hw5-junior-link"}
+              to={PATH.JUNIOR}
+              onClick={handleClose}
+              className={({ isActive }) => (isActive ? s.active : "")}
+            >
+              Junior
+            </NavLink>
+            <NavLink
+              id={"hw5-junior-plus-link"}
+              to={PATH.JUNIOR_PLUS}
+              onClick={handleClose}
+              className={({ isActive }) => (isActive ? s.active : "")}
+            >
+              Junior Plus
+            </NavLink>
+          </nav>
+        </aside>
+      )}
     </>
   );
 };
