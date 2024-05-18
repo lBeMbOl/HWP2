5323d28cb2c6d84f5f9daa2c51e9298ae76697f7
lBeMbOl <lbembol@mail.ru>
2024-05-18 13:40:01 +0500

    HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,55 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: "sort"; payload: "up" | "down" }
+  | { type: "check"; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
+export const homeWorkReducer = (
+  state: UserType[],
+  action: ActionType
+): UserType[] => {
+  // need to fix any
+  switch (action.type) {
+    case "sort": {
+      let copystate = [...state];
+      if (action.payload == "up") {
+        copystate.sort((a, b) => {
+          const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+          const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
 
-            return state // need to fix
+          // names must be equal
+          return 0;
+        });
+
+        return copystate;
+      }
+      copystate.sort((a, b) => {
+        const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+        const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+        if (nameA > nameB) {
+          return -1;
         }
-        case 'check': {
-
-            return state // need to fix
+        if (nameA < nameB) {
+          return 1;
         }
-        default:
-            return state
+
+        // names must be equal
+        return 0;
+      });
+      return copystate; // need to fix
     }
-}
+    case "check": {
+      let copystate = [...state];
+
+      return copystate.filter((t) => t.age > 17); // need to fix
+    }
+    default:
+      return state;
+  }
+};
